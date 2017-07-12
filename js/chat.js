6355b0940690841a67e6cd34b6ec8bf4491775ce
Enr1g <enr1g@outlook.com>
2017-07-12 11:00:50 +0300

    Send message on enter

--- js/chat.js
+++ js/chat.js
@@ -60,7 +60,7 @@ require(['jquery', 'client', 'utils', 'events'], function($, client, utils, $_) 
     });
 
     $('#messageText').on('keypress', function(e) {
-        if (e.keyCode === 13 && (e.metaKey || e.ctrlKey)) {
+        if (e.keyCode === 13) {
             $('#sendMessage').click();
         }
     });
